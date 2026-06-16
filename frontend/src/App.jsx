3f33af10f12c8fd1bb9d484b351668741b53ce83
Favio Britos <faviobritos5@gmail.com>
2026-06-16 17:11:22 -0300

    Home inicial Abogapp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Abogapp</h1>
+
+      <h2>Encontrá al abogado adecuado</h2>
+
+      <div className="buttons">
+        <button>BUSCAR ABOGADO</button>
+        <button>SOY ABOGADO</button>
+        <button>Iniciar sesión</button>
+        <button>Registrarme</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
