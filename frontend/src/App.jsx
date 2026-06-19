683509482ee1a082a60bb20433aee3ddee1fa9d7
Favio Britos <faviobritos5@gmail.com>
2026-06-19 09:54:35 -0300

    Configuracion inicial de rutas y paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+
+import Home from "./pages/Home";
+import BuscarAbogado from "./pages/BuscarAbogado";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
+import SoyAbogado from "./pages/SoyAbogado";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Abogapp</h1>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/buscar" element={<BuscarAbogado />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/soy-abogado" element={<SoyAbogado />} />
+        </Routes>
 
-      <h2>Encontrá al abogado adecuado</h2>
-
-      <div className="buttons">
-        <button>BUSCAR ABOGADO</button>
-        <button>SOY ABOGADO</button>
-        <button>INICIAR SESIÓN</button>
-        <button>REGISTRARME</button>
+        <div className="buttons">
+          <Link to="/buscar"><button>BUSCAR ABOGADO</button></Link>
+          <Link to="/soy-abogado"><button>SOY ABOGADO/A</button></Link>
+          <Link to="/login"><button>INICIAR SESIÓN</button></Link>
+          <Link to="/registro"><button>REGISTRARME</button></Link>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
